e7fdf69632789e6f88cb6388909f3238fd303113
Turhan Oz <@turhanoz>
2015-10-31 11:07:29 +0100

    add BeaconsAttachementService based on API

---
+++ library/src/main/java/com/turhanoz/android/rxproximitybeacon/BeaconsAttachmentService.java
@@ -0,0 +1,41 @@
+package com.turhanoz.android.rxproximitybeacon;
+
+import com.turhanoz.android.rxproximitybeacon.model.BeaconAttachment;
+import com.turhanoz.android.rxproximitybeacon.model.ListAttachments;
+
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
+import retrofit.http.Query;
+import rx.Observable;
+
+// TODO : There is a known issue with retrofit's path including "/"
+// Fix all calls listed below, replacing beacons/{beaconName}
+// by {beaconName} (which is stored in Beacon.beaconName
+// https://github.com/square/retrofit/issues/1168
+// http://stackoverflow.com/questions/33127914/retrofit-2-0-beta-2-issue-with-path-encoding
+public interface BeaconsAttachmentService {
+    // TODO : fix path (see description)
+    @POST("beacons/{beaconName}/attachments:batchDelete")
+    Observable<Integer> batchDelete(@Path("beaconName") String beaconName, @Query("namespacedType") String nameSpacedType);
+
+    // TODO : fix path (see description)
+    @POST("beacons/{beaconName}/attachments")
+    Observable<BeaconAttachment> create(@Path("beaconName") String beaconName, @Body BeaconAttachment beaconAttachment);
+
+    // TODO : fix path (see description)
+    @POST("beacons/{beaconName}/attachments/{attachmentName}")
+    Observable<Void> delete(@Path("beaconName") String beaconName, @Path("attachmentName") String attachmentName);
+
+
+    // TODO : fix path (see description)
+    @GET("beacons/{beaconName}/attachments?namespacedType=*/*")
+    Observable<ListAttachments> list(@Path("beaconName") String beaconName);
+
+    // TODO : fix path (see description)
+    @GET("beacons/{beaconName}/attachments")
+    Observable<ListAttachments> list(@Path("beaconName") String beaconName, @Query("namespacedType") String nameSpacedType);
+
+
+}
